a8058061413677dd0b41a2624e51f2bcb9f281b9
daiyi <clsun91@gmail.com>
2015-02-16 16:21:44 -0800

    added model for little guy

--- guytracker/models.py
+++ guytracker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Lilguy(models.Models):
+    """ A little guy ready for adventure. """
+
+    # unique key code for editing the little guy
+    code = models.CharField(max_length=22, unique=True)
+    # the name of the little guy
+    name = models.CharField(max_length=55)
+    # the icon/avatar photo of the little guy
+    pic = models.ImageField(upload_to="lilguybook", max_length=200)    
+    # longitude and latitude positions of most recently logged location
+    current_lon = models.FloatField()
+    current_lat = models.FloatField()
+
