311e7323b0c97af6b3cb2910875aef6ca10dba7b
daiyi <clsun91@gmail.com>
2015-02-16 21:50:56 -0800

    greating views and lilguy page template

--- guytracker/views.py
+++ guytracker/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from guytracker import Lilguy, Chapter
+from django.http import HttpResponse
+
+
+def display_guy(request, lilguy_name):
+    """ Returns an HttpResponse for an individual lilguy's information \
+        and chapters page. """
+
+    # finds all Chapters about lilguy_name
+    chapters = (Chapter.objects.select_related()
+                .filter(lilguy__name=lilguy_name)
+                .order_by('-date_inserted'))
+
+    lilguy = None
+    if len(chapters) > 0:
+        lilguy = chapters[0].lilguy
+    else:
+        return render_to_response('error.html')
+
+    return (render_to_response('display_guy.html', 
+                               {'lilguy': lilguy, 'chapters': chapters}))
