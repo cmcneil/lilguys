54c74177491848c85cf3699f96762c8aeffbf438
Carson McNeil <carson@leviathan.convox.org>
2015-02-26 23:40:50 -0800

    Got code authentication AJAX call working. This call will be used to unhide the edit form when the user enters the code.

--- guytracker/ajax.py
+++ guytracker/ajax.py
@@ -1,8 +1,13 @@
 from dajaxice.decorators import dajaxice_register
-from django.utils import json
+import guytracker.utils as ut
+import json
 
 @dajaxice_register
-def auth_lilguy_code(request):
-    return json.dumps({'message': 'Hello!'})
+def auth_lilguy_code(request, url_code, auth_code):
+    lilguy_id = ut.urlsafe_code_to_lilguy_id(url_code)
+    actual_auth_code = ut.lilguy_id_to_activation_code(lilguy_id)
+    if auth_code == actual_auth_code:
+        return json.dumps({'status': 'VALID'})
+    return json.dumps({'status': 'INVALID'})
 
 
