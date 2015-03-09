d407ac85a99c23e8b4a28110b915aa7abbe39907
Carson McNeil <carson@leviathan.convox.org>
2015-03-09 01:34:21 -0700

    Updated model to save image thumbs

--- guytracker/models.py
+++ guytracker/models.py
@@ -1,5 +1,7 @@
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
-# Create your models here.
+from PIL import Image
+import StringIO
 
 class Lilguy(models.Model):
     """ A little guy ready for adventure. """
@@ -45,6 +47,31 @@ class Chapter(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     # Submiter's email(if subscribed)
     email = models.EmailField(max_length=254, null=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        """Override save to resize picture field."""
+        if self.picture:
+            EXTRA_IMAGE_SIZES = {'picture_600': (600, 900)}
+            MAX_SIZE = (1280, 1280)
+            imgFile = Image.open(self.image.path)
+            #Convert to RGB
+            if imgFile.mode not in ('L', 'RGB'):
+                imgFile = imgFile.convert('RGB')
+            for field_name, size in EXTRA_IMAGE_SIZES:
+                field = getattr(self, field_name)
+                working = imgFile.copy()
+                working.thumbnail(size, Image.ANTIALIAS)
+                fp = StringIO()
+                working.save(fp, format="JPEG", quality=95)
+                cf = ContentFile(fp.getvalue())
+                field.save(name=self.image.name, content=cf, save=False)
+            imgFile.thumbnail(MAX_SIZE, Image.ANTIALIAS)
+            output = StringIO.StringIO()
+            imgFile.save(output, format="JPEG", quality=95)
+            output.seek(0)
+            self.picture = File(output, self.picture.name())
+
+        super(Chapter, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return ("Chapter(title=" + self.title + 
